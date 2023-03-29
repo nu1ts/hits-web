daeb17fb1a3ed716ae2c1bb5d452db1576ad7a27
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-03-29 13:50:47 +0700

    delete last commit
    Надо было с компа на ноут код перекинуть, поэтому сделал этот коммит. Для этого как раз сделал доп ветку, чтобы не засорять в develop-ветке.

--- scripts/A_star/A_star.js
+++ scripts/A_star/A_star.js
@@ -1,12 +1,1 @@
-class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }
-  
-    greet() {
-      console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
-    }
-}
-const person1 = new Person('John', 30);
-person1.greet();
+
