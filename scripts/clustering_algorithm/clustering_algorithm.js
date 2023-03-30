0de671296a82aca59ec9a4a2bc826d1eba4badc7
unknown <arsen190104@gmail.com>
2023-03-31 00:06:09 +0700

    canvas

--- scripts/clustering_algorithm/clustering_algorithm.js
+++ scripts/clustering_algorithm/clustering_algorithm.js
@@ -1,1 +1,17 @@
+function windowToCanvas(canvas, x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return { x: x - bbox.left * (canvas.width / bbox.width), y: y - bbox.top * (canvas.height / bbox.height) };
+}
 
+var canvas = document.getElementById("canvas1");
+    ctx     = canvas.getContext('2d');
+canvas.width = document.documentElement.clientWidth*0.6;
+canvas.height = document.documentElement.clientHeight*0.6;
+
+canvas.addEventListener('mousedown',
+function(e) {
+    var loc = windowToCanvas(canvas, e.clientX, e.clientY);
+    ctx.beginPath();
+    ctx.arc(loc.x, loc.y, 10, 0, Math.PI*2);
+    ctx.fill();
+});
