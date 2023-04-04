fecfcb264133cf0615aeaad8a1c042fd20ea19a6
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-04-05 01:18:35 +0700

    adaptive canvas

--- scripts/aStar/aStar.js
+++ scripts/aStar/aStar.js
@@ -1,21 +1,5 @@
-function openAStar(evt, algorithmName) {
-  var i, tabContent, tablinks;
+import { makeWall } from "./aStarCanvas";
 
-  tabContent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }   
-
-  document.getElementById(algorithmName).style.display = "inline-block";
-  evt.currentTarget.className += " active";
-}
-
-//code
 class Cell {
     constructor(x, y, isOccupied) {
       this.x = x;
@@ -37,25 +21,25 @@ function createMap(width, height) {
       map[w] = new Array(height);
       for (let h = 0; h < height; h++) {
         map[w][h] = new Cell(w, h, false);
-        map[w][h].makeWall();
+        makeWall(map, w, h);
       }
     }
+}
 
-    let x = Math.floor(Math.random() * width / 2) * 2 + 1;
-    let y = Math.floor(Math.random() * height / 2) * 2 + 1;
-    map[x][y].clear_cell();
+let x = Math.floor(Math.random() * width / 2) * 2 + 1;
+let y = Math.floor(Math.random() * height / 2) * 2 + 1;
+map[x][y].clear_cell();
 
-    let to_check = new Array();
-    if (y - 2 >= 0) {
-      to_check.push(new Point(x, y - 2));
-    }
-    if (y + 2 < height) {
-      to_check.push(new Point(x, y + 2));
-    }
-    if (x - 2 >= 0) {
-      to_check.push(new Point(x - 2, y));
-    }
-    if (x + 2 < width) {
-      to_check.push(new Point(x + 2, y));
-    }
+let to_check = new Array();
+if (y - 2 >= 0) {
+    to_check.push(new Point(x, y - 2));
+}
+if (y + 2 < height) {
+    to_check.push(new Point(x, y + 2));
+}
+if (x - 2 >= 0) {
+    to_check.push(new Point(x - 2, y));
+}
+if (x + 2 < width) {
+    to_check.push(new Point(x + 2, y));
 }
