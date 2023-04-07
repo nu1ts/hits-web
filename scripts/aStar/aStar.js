8e4eeb1978307c97eb68489f25cf1f0d454cbf61
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-04-07 09:55:13 +0700

    last code

--- scripts/aStar/aStar.js
+++ scripts/aStar/aStar.js
@@ -4,6 +4,16 @@ class Cell {
       this.y = y;
       this.isOccupied = isOccupied;
     }
+
+    makeWall() {
+      var canvas = document.getElementById("astar-canvas");
+      var ctx = canvas.getContext("2d");
+      var wall = new Image();
+      wall.onload = function() {
+        ctx.drawImage(wall, this.x, this.y, this.x, this.y);
+      }
+      wall.src = 'images/wall.png';
+    }
 }
 
 class Point {
@@ -20,45 +30,19 @@ window.addEventListener('load', function() {
   divContainer.appendChild(canvas);
 });
 
-function makeWall(x, y, width, height) {
-  var canvas = document.getElementById("astar-canvas");
-  var ctx = canvas.getContext("2d");
-  var wall = new Image();
-  wall.onload = function() {
-    ctx.drawImage(wall, x, y, width / 10, height / 10);
-  }
-  wall.src = 'images/wall.png';
-}
-
 function clearCell(x, y, width, height) {
   
 }
 
 function createMap(width, height) {
   let map = new Array(width);
-  for (let w = 0; w < width; w++) {
-    map[w] = new Array(height);
-    for (let h = 0; h < height; h++) {
-      map[w][h] = new Cell(5, 5, false);
-      makeWall(0, 0);
+  let cellWidth = width * 30 / 100;
+  let cellHeight = height * 30 / 100;
+  for (let x = 0; x < width; x + cellWidth) {
+    map[x] = new Array(height);
+    for (let y = 0; y < height; y + cellHeight) {
+      map[x][y] = new Cell(x, y, true);
+      map[x][y].makeWall();
     }
   }
-
-  /*let x = Math.floor(Math.random() * width / 2) * 2 + 1;
-  let y = Math.floor(Math.random() * height / 2) * 2 + 1;
-  clearCell();
-
-  let to_check = new Array();
-  if (y - 2 >= 0) {
-    to_check.push(new Point(x, y - 2));
-  }
-  if (y + 2 < height) {
-    to_check.push(new Point(x, y + 2));
-  }
-  if (x - 2 >= 0) {
-    to_check.push(new Point(x - 2, y));
-  }
-  if (x + 2 < width) {
-    to_check.push(new Point(x + 2, y));
-  }*/
 }
